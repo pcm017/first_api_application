343384d84d930fe79d2b7fcf176017a9ef86f56c
pratikmehta <pratikmehta@CVMUM010.creditvidya.local>
2020-04-19 14:28:47 +0530

    Added root webpage

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ api.add_resource(Store,'/store/<string:name>')
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList,'/items')
 api.add_resource(UserRegister,'/createUser')
-#api.add_resource(website,'/')
+api.add_resource(website,'/')
 
 if __name__ == "__main__":
     
